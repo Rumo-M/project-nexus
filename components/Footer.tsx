b1fc35b83f44f5d4282d9202ab55d9141c9343c3
Martha Maloba <marthamarumo92@gmail.com>
2025-09-12 21:32:44 +0200

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+// components/Footer.tsx
+export default function Footer() {
+  return (
+    <footer className="bg-gray-200 text-gray-700 p-4 text-center mt-8">
+      <p>&copy; 2025 Project Nexus. All rights reserved.</p>
+    </footer>
+  );
+}
